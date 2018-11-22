8200c7f1d2c796b097cb022d93ec653c5a859f96
Higlyx <minecraft.99999@mail.ru>
2018-11-22 23:17:22 +0900

    Created list of points

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,11 +11,24 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-            
+
+            Point p3 = new Point(3, 2, '^');
+
+            Point p4 = new Point(5, 1, '&');
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach( Point i in pList)
+            {
+                i.Draw();
+            }
+
             Console.ReadLine();
         }
     }
