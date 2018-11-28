1ea0fc4f3e928d6149e1b32d4ead8b4af961f99c
Higlyx <minecraft.99999@mail.ru>
2018-11-28 15:17:33 +0900

    Created Figure Snake

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,6 +11,7 @@ namespace Snake
         static void Main(string[] args)
         {
             Console.SetBufferSize(120, 30);
+            //Отрисовка границ
             Horizontal Top = new Horizontal(1, 119, 1, '#');
             Horizontal Bottom = new Horizontal(1, 119, 29, '#');
             Vertical Left = new Vertical(1, 0, 28, '#');
@@ -20,13 +21,18 @@ namespace Snake
             Left.Draw();
             Right.Draw();
 
-            Point p1 = new Point(5, 5, '*');
+            //Отрисовка точек
+            Point p1 = new Point(5, 5, 'o');
             p1.Draw();
-            Point p2 = new Point(6, 8, '*');
+            Point p2 = new Point(6, 8, 'o');
             p2.Draw();
-            Point p3 = new Point(10, 5, '*');
+            Point p3 = new Point(10, 5, 'o');
             p3.Draw();
 
+            //Отрисовка змейки
+            Point s = new Point(5, 10, '*');
+            Snake snake = new Snake(s, 4, Direction.UP);
+            snake.Draw();
 
             Console.ReadLine();//Написать класс горизонтальных линий и класс вертикальных
         }
